acf8cdedb8b5a152984d6db3b4a396856999e9e5
user <user@BULLSEYE-debian-live-builder>
2022-05-05 14:37:07 -0400

    update

---
+++ js/head.js
@@ -0,0 +1,12 @@
+document.write(`
+<meta charset="utf-8">
+<title>Alvaro Pintado</title>
+<link rel="stylesheet" href = "/css/style.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Spectral" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"> 
+
+<!-- LaTex Rendering with MathJax-->
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+`);
