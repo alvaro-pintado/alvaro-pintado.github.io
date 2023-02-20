e08dd8e011a9eb769149fd799cce7354e2d9fcc9
alvaro-pintado <user@BULLSEYE-debian-live-builder>
2023-02-19 23:49:07 -0500

    update

--- js/blogHead.js
+++ js/blogHead.js
@@ -1,11 +1,12 @@
 document.write(`
-    <meta charset="utf-8">
-    <title>Alvaro Pintado</title>
-    <link rel="stylesheet" href = "/css/blog.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet"> 
+<meta charset="utf-8">
+<title>Alvaro Pintado</title>
+<link rel="stylesheet" href = "/css/blog.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Cardo " rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Libre Baskerville" rel="stylesheet"> 
 
-    <!-- LaTex Rendering with MathJax-->
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<!-- LaTex Rendering with MathJax-->
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 `);
