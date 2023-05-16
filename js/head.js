7563fdc188ead1922057c94490b3e9440bc4d7fd
alvaro-pintado <user@BULLSEYE-debian-live-builder>
2023-05-16 13:46:19 -0400

    update

--- js/head.js
+++ js/head.js
@@ -6,6 +6,8 @@ document.write(`
 <link href="https://fonts.googleapis.com/css?family=Cardo " rel="stylesheet"> 
 <link href="https://fonts.googleapis.com/css?family=Libre Baskerville" rel="stylesheet"> 
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 <!-- LaTex Rendering with MathJax-->
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
